664aca1edbbc77b7d48dd14feed4e46719fdcd60
SHIREL16 <shirulalo2003@gmail.com>
2022-05-12 16:44:03 -0300

    hice cambios

--- archivo4.js
+++ archivo4.js
@@ -1,1 +1,11 @@
 console.log("archivo4");
+ /*crear el repositoria entro a una carpeta abro git y pongo git clone y pego lo del otro 
+desp vamos a visual y entro a la carpeta donde clonecodeo algo y en la terminal pongo git status(para ver los cambios q no modificamos)
+hago mi git commit-m flor con un comentarios entre " para subir archivos
+primero hacer git pool para integrar los cambis q hicimos en la compu
+primero comit desp pool y despo push
+cuando se baja el contenido con pool(actualizamos)
+
+no es conveniente hacer pool si no tenes todo comitiado
+comit guardado
+pool es para integrar los cambios */
